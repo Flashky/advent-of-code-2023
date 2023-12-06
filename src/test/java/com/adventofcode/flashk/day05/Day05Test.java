2e94f528a7f463941a93ae233895a19d3ef52d2c
Flashk <mevengar@gmail.com>
2023-12-06 20:38:17 +0100

    Add comments to @Disabled tests and remove public modifiers to tests

--- src/test/java/com/adventofcode/flashk/day05/Day05Test.java
+++ src/test/java/com/adventofcode/flashk/day05/Day05Test.java
@@ -55,8 +55,8 @@ public class Day05Test extends PuzzleTest {
 	@Tag(TestTag.PART_ONE)
 	@Tag(TestTag.SAMPLE)
 	@DisplayName(TestDisplayName.PART_ONE_DEBUG)
-	@Disabled
-	public void testSolvePart1SampleTest() {
+	@Disabled("Sample test just for analysing the behaviour for part 2.")
+	void testSolvePart1SampleTest() {
 
 		System.out.print("1 | sample | ");
 
@@ -105,7 +105,7 @@ public class Day05Test extends PuzzleTest {
 	@Tag(TestTag.PART_TWO)
 	@Tag(TestTag.INPUT)
 	@DisplayName(TestDisplayName.PART_TWO_INPUT)
-	@Disabled
+	@Disabled("Solved via brute force. Requires 30 minutes to run.")
 	public void testSolvePart2Input() {
 		
 		System.out.print("2 | input  | ");
